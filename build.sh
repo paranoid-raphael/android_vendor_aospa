8c7dd53df45f14a82e5358bafb0520304a0bac8c
Abhiram <103818282+Azure-Helper@users.noreply.github.com>
2022-06-12 05:40:34 +0530

    [DNM] build.sh: Fix copying error of final zip
    Signed-off-by: Azure-Helper <azurehelper219@gmail.com>

--- build.sh
+++ build.sh
@@ -280,7 +280,7 @@ else
 
     checkExit
 
-    cp -f $OUT/aospa_$DEVICE-ota-$FILE_NAME_TAG.zip $OUT/aospa-$AOSPA_VERSION.zip
+    cp -f $OUT/aospa_$DEVICE-ota-eng.arm.zip $OUT/aospa-$AOSPA_VERSION.zip
     echo "Package Complete: $OUT/aospa-$AOSPA_VERSION.zip"
 fi
 echo -e ""
